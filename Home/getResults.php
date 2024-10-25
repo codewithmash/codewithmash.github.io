b8feb6308857ba78d053c7f46b55b0ae5f7cfd6e
codewithmash <78247111+codewithmash@users.noreply.github.com>
2024-10-25 22:51:22 +0530

    Update getResults.php

--- Home/getResults.php
+++ Home/getResults.php
@@ -26,45 +26,45 @@ try{
 
 
     $client = new MongoDB\Client("mongodb+srv://user_me:6mF31Y0gPns9MyWZ@cluster0.fowhw.mongodb.net/yatharthOnlineClasses?retryWrites=true&w=majority&appName=Cluster0");
-    $collection = $client->yatharthOnlineClasses->{"res_$year"}; // 'school_results' is the database name, 'results' is the collection name
+    // $collection = $client->yatharthOnlineClasses->{"res_$year"}; // 'school_results' is the database name, 'results' is the collection name
 
-    if (!$rollNumber || !$rollCode ) {
-        echo json_encode(["success" => false, "message" => "Roll number and roll code are required"]);
-        exit();
-    }
-   
-    // $documents = $collection->find();
-
-    // $data = [];
-    // foreach ($documents as $document) {
-    //     $data[] = json_decode(json_encode($document), true);
+    // if (!$rollNumber || !$rollCode ) {
+    //     echo json_encode(["success" => false, "message" => "Roll number and roll code are required"]);
+    //     exit();
     // }
-    // echo json_encode($data, JSON_PRETTY_PRINT);
+   
+    // // $documents = $collection->find();
 
-    // // Query MongoDB for the result
+    // // $data = [];
+    // // foreach ($documents as $document) {
+    // //     $data[] = json_decode(json_encode($document), true);
+    // // }
+    // // echo json_encode($data, JSON_PRETTY_PRINT);
+
+    // // // Query MongoDB for the result
     
-    $query = ['roll_number' => $bc, 'roll_code' => $ab];
-    $result = $collection->findOne($query);
+    // $query = ['roll_number' => $bc, 'roll_code' => $ab];
+    // $result = $collection->findOne($query);
 
-    // echo json_encode(["success" => false, "message" => $data[0]],JSON_PRETTY_PRINT);
+    // // echo json_encode(["success" => false, "message" => $data[0]],JSON_PRETTY_PRINT);
 
-    // $new  = json_decode(json_encode($result), true);
-    // $me = json_encode($new, JSON_PRETTY_PRINT);
-    // echo json_encode(["success" => true, "result" => $me["roll_number"]]);
+    // // $new  = json_decode(json_encode($result), true);
+    // // $me = json_encode($new, JSON_PRETTY_PRINT);
+    // // echo json_encode(["success" => true, "result" => $me["roll_number"]]);
     
 
-    if ($result) {
+    // if ($result) {
 
-        // echo json_encode(["success" => true, "result" => "found"]);
+    //     // echo json_encode(["success" => true, "result" => "found"]);
 
-        $new  = json_decode(json_encode($result), true);
-        // $me = json_encode($new, JSON_PRETTY_PRINT);
-        echo json_encode(["success" => true, "result" => $new["pdf_one"]]);
-    } 
+    //     $new  = json_decode(json_encode($result), true);
+    //     // $me = json_encode($new, JSON_PRETTY_PRINT);
+    //     echo json_encode(["success" => true, "result" => $new["pdf_one"]]);
+    // } 
 
-    else {
-        echo json_encode(["success" => false, "message" => "Result not found"]);
-    }
+    // else {
+    //     echo json_encode(["success" => false, "message" => "Result not found"]);
+    // }
 
 }
 
